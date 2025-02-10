e1054657a70f61e82c45870a08d2f3363bb3e002
YashdeepN <yashdeepneema@hotmail.com>
2025-02-10 00:08:52 -0600

    Solve 07-temp-Conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+  return Number(((tempF - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  return Number((tempC * (9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
+
+console.log(convertToCelsius(100));
