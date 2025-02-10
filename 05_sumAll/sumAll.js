5b9c62c69a4ca0ead3023af7f61cb8dc1ae61260
YashdeepN <yashdeepneema@hotmail.com>
2025-02-09 23:50:57 -0600

    Solve 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  )
+    return "ERROR";
 
+  if (num1 > num2) {
+    [num1, num2] = [num2, num1];
+  }
+  let sum = 0;
+  const LOOP_LENGTH = num2 - num1;
+  for (let i = num1; i <= num2; ++i) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+console.log(sumAll(123, 1));
