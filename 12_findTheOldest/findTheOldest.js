44faf58b7abfc399ab788dda1cf1da8a9759d653
YashdeepN <yashdeepneema@hotmail.com>
2025-02-12 08:06:07 -0600

    Solve 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+function getAge(birthYear, deathYear) {
+  if (!deathYear) {
+    deathYear = new Date().getFullYear();
+  }
+  return deathYear - birthYear;
+}
 
+const findTheOldest = function (people) {
+  const oldest = people.reduce(
+    (max, person) =>
+      getAge(person.yearOfBirth, person.yearOfDeath) > max
+        ? (max = getAge(person.yearOfBirth, person.yearOfDeath))
+        : (max = max),
+    0
+  );
+  const oldestPerson = people.filter(
+    (person) => getAge(person.yearOfBirth, person.yearOfDeath) === oldest
+  )[0];
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
